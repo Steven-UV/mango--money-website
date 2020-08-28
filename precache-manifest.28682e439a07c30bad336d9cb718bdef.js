ffcf147b2c85ebb2dbe69fd0760eaf255c319a01
steven <1938848188@qq.com>
2020-08-28 20:42:25 +0800

    deploy

---
+++ precache-manifest.28682e439a07c30bad336d9cb718bdef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ccec26637e5da092e21b14de2fbb43e",
+    "url": "/mango--money-website/index.html"
+  },
+  {
+    "revision": "756a59110972cae23c79",
+    "url": "/mango--money-website/static/css/main.6ac2eca7.chunk.css"
+  },
+  {
+    "revision": "0961a0ba3d15b264df3f",
+    "url": "/mango--money-website/static/js/2.ac976310.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/mango--money-website/static/js/2.ac976310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756a59110972cae23c79",
+    "url": "/mango--money-website/static/js/main.5df363d5.chunk.js"
+  },
+  {
+    "revision": "ed07bb6cc4d9d0cffc13",
+    "url": "/mango--money-website/static/js/runtime-main.4f524ed4.js"
+  }
+]);
